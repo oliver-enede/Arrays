9dbc207bb0c39679cf2bcf1c86bbeb1e004dfbb3
oliver-enede <78216854+oliver-enede@users.noreply.github.com>
2024-06-03 15:52:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] obj = { 12, 2.5, 8, 5.5, "monday", "tuesday" };
+
+            double sum = 0;
+            foreach (var item in obj)
+            {
+                if (item is int)
+                {
+                    sum += (int)item;
+                }
+                else if (item is double)
+                {
+                    sum += (double)item;
+                }
+            }
+
+            Console.WriteLine($"Sum of integers and doubles: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
